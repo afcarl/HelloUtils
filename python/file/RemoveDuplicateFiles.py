79a6578f1f0840015d96311a925248ef49c7f5ee
handong1587 <handong1587@163.com>
2017-03-01 19:28:53 +0800

    refine python code

--- python/file/RemoveDuplicateFiles.py
+++ python/file/RemoveDuplicateFiles.py
@@ -2,9 +2,11 @@
 
 import md5
 import os
+import sys
 from time import clock as now
+
 def getmd5(filename):
-    file_txt = open(filename,'rb').read()
+    file_txt = open(filename, 'rb').read()
     m = md5.new(file_txt)
     return m.hexdigest()
 
@@ -12,53 +14,63 @@ def find(s, ch):
     return [i for i, ltr in enumerate(s) if ltr == ch]
     
 def getNewFile(oldfile):
-    index=find(oldfile,'.')[-1]
-    newfile=oldfile[:index]+'(1)'+oldfile[index:]
+    index = find(oldfile,'.')[-1]
+    newfile = oldfile[:index] + '(1)' + oldfile[index:]
     return newfile
     
-def main():
-    #path = raw_input("path: ")
-    path = 'E:/path_to_files'
+def readConfigFile(filename):
+    f = open(filename, 'r')
+    path_list = [line.rstrip('\n') for line in f]
+    f.close()
+    return path_list
+
+def run(path_list):
     all_md5 = {}
     all_size = {}
-    total_file=0
-    total_delete=0
-    start=now()
-    # only remove file name as 'aaa(1).jpg'
-    for file in os.listdir(path):
-        total_file += 1
-        real_path=os.path.join(path,file)
-        #print real_path
-        if os.path.isfile(real_path) == True:
-            size = os.stat(real_path).st_size
-            name_and_md5=[real_path,'']
-            if size in all_size.keys():
-                new_md5 = getmd5(real_path)
-                if all_size[size][1]=='':
-                    all_size[size][1]=getmd5(all_size[size][0])
-                if new_md5 in all_size[size]:
-                    print 'Remove ',file
-                    #os.remove(real_path)
-                    total_delete += 1
-                    
-                    # my personal setting
-                    '''
-                    target_file=getNewFile(file)
-                    target_path = os.path.join(path,target_file)
-                    if os.path.isfile(target_path) == True:
-                        print 'Remove ',target_file
-                        os.remove(target_path)
+    total_file = 0
+    total_delete = 0
+    start = now()
+
+    for path in path_list:
+        for file in os.listdir(path):
+            total_file += 1
+            real_path = os.path.join(path, file)
+            if os.path.isfile(real_path) == True:
+                size = os.stat(real_path).st_size
+                name_and_md5 = [real_path,'']
+                if size in all_size.keys():
+                    new_md5 = getmd5(real_path)
+                    if all_size[size][1] == '':
+                        all_size[size][1] = getmd5(all_size[size][0])
+                    if new_md5 in all_size[size]:
+                        print('Remove {}'.format(real_path))
+                        os.remove(real_path)
                         total_delete += 1
-                    '''
+                        
+                        # my personal setting: only remove file name as 'aaa(1).jpg'
+                        '''
+                        target_file = getNewFile(file)
+                        target_path = os.path.join(path, target_file)
+                        if os.path.isfile(target_path) == True:
+                            print('Remove {}'.format(target_file))
+                            os.remove(target_path)
+                            total_delete += 1
+                        '''
+                    else:
+                        all_size[size].append(new_md5)
                 else:
-                    all_size[size].append(new_md5)
-            else:
-                all_size[size]=name_and_md5
+                    all_size[size] = name_and_md5
     end = now()
     time_last = end - start
-    print 'Total files:   ',total_file
-    print 'Remove files:  ',total_delete
-    print 'Time consumed: ',time_last,' seconds'
+    print('Total files:   {}'.format(total_file))
+    print('Remove files:  {}'.format(total_delete))
+    print('Time consumed: {} seconds'.format(time_last))
      
-if __name__=='__main__': 
-    main()
+if __name__ == '__main__':
+    if len(sys.argv[1]) < 2:
+        print('Not input file specified.')
+        sys.exit()
+
+    input_file = sys.argv[1]
+    path_list = readConfigFile(input_file)
+    run(path_list)
